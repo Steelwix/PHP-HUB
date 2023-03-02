2d929c4b8de34eb9323fcd0ceb4a91afa0d477c2
Steelwix <mhunmael@hotmail.com>
2023-03-02 19:23:28 +0100

    first home page

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,8 @@ import { createApp } from 'vue';
 import App from './js/App.vue';
 import Graf from './js/Graf.vue';
 import Dyma from './js/Dyma.vue';
+import Home from './js/Home.vue';
 createApp(App).mount('#vue-app');
 createApp(Graf).mount('#vue-grafikart');
 createApp(Dyma).mount('#vue-dyma');
+createApp(Home).mount('#vue-home');
---
+++ assets/js/Home.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>PHP HUB</h1>
+                <p>All you need about dev</p>
+            </div>
+        </div>
+    </div>
+</template>
