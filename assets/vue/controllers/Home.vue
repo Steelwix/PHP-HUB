468dafeb25e657a7d0a643839a7c7778160ba94b
Steelwix <mhunmael@hotmail.com>
2023-03-03 17:44:29 +0100

    base html fix

--- assets/vue/controllers/Home.vue
+++ assets/vue/controllers/Home.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>Hello {{ name }}!</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>PHP HUB</h1>
+                <p>All you need about dev, {{ name }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
 defineProps({
-    name: String
+    name: String,
+    services: Array
 });
+console.log(name)
+
 </script>
